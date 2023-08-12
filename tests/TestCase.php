f6ebe3808b0aa7d0ce977e5635e20e712c767f8f
afatmustafa <afatmustafa@users.noreply.github.com>
2023-08-12 22:14:35 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,7 @@
 
 namespace Afatmustafa\FilamentTurnstile\Tests;
 
+use Afatmustafa\FilamentTurnstile\FilamentTurnstileServiceProvider;
 use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
 use Filament\Actions\ActionsServiceProvider;
@@ -18,7 +19,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use Afatmustafa\FilamentTurnstile\FilamentTurnstileServiceProvider;
 
 class TestCase extends Orchestra
 {
